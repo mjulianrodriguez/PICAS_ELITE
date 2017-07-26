081408baf1c99eccee43aa2d50de716105ecf8f7
kleer <kleer@kleer-csd-box>
2017-07-26 09:48:43 -0500

    app.rb

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require 'sinatra'
 require './config'
 
 get '/' do
+	erb(:index)
+end
 
+post '/jugar' do
+	session['numeroIntento'] = "#{params['numeroJugado']}"
+	session['resultado']="Picas 0 - Fijas 0 #{params['numeroJugado']}"
+	erb(:index)
 end
